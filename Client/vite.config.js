66341fa27d1273e65faa783e0d59ce230e5e51a4
Konan <kixeyems0@gmail.com>
2024-04-30 18:21:23 +0100

    added vite config

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4242', // Your API server address
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
